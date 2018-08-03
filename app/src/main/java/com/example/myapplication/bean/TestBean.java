7d34fa8e5fb53a981bf8623f34079fadc32926f8
Johnny Liu <johnnyliu.dev@gmail.com>
2018-08-03 18:01:46 +0800

    test

---
+++ app/src/main/java/com/example/myapplication/bean/TestBean.java
@@ -0,0 +1,44 @@
+package com.example.myapplication.bean;
+
+/**
+ * Description:
+ * Detail:
+ * Create Time: 2018/8/3
+ *
+ * @author LiuFangQiang
+ * @version 1.0
+ * @see ...
+ * History:
+ * @since Since
+ */
+public class TestBean {
+
+    private String name;
+    private String gender;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
