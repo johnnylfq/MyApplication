1d8619de5e322e0d4ea4f7da593f2605e99c59d0
Johnnyliu <johnnyliu.dev@gmail.com>
2018-08-08 18:23:24 +0800

    配置gradle文件

---
+++ app/src/main/java/com/example/myapplication/base/activity/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication.base.activity;
+
+/**
+ * Description:
+ * Detail:
+ * Create Time: 2018/8/8
+ *
+ * @author LiuFangQiang
+ * @version 1.0
+ * @see ...
+ * History:
+ * @since Since
+ */
+public class BaseActivity {
+}
---
+++ app/src/main/java/com/example/myapplication/base/view/AbstractView.java
@@ -0,0 +1,26 @@
+package com.example.myapplication.base.view;
+
+/**
+ * Description:
+ * Detail:
+ * Create Time: 2018/8/8
+ *
+ * @author LiuFangQiang
+ * @version 1.0
+ * @see ...
+ * History:
+ * @since Since
+ */
+public interface AbstractView {
+
+    void showNormal();
+
+    void showError();
+
+    void showLoading();
+
+    void showEmpty();
+
+    void showNeteaseLoading();
+
+}
